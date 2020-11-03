bd1b8eb215684e59992385845476d2023131a946
joematune <joematune@gmail.com>
2020-11-03 15:05:28 +0200

    Finalize initial commit

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import Alert from '../components/alert'
 import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
 
 export default function Home({ allPostsData }) {
 
@@ -18,13 +20,18 @@ export default function Home({ allPostsData }) {
           This is a learning experience for Next.js as a framework. {' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+
+
+
+        {/*
+          Great opportunity to use SWR https://swr.vercel.app/
+          Fetch a dev joke, display it as me
+        */}
         <Alert type="error"> error!</Alert>
         <Alert type="success">success!</Alert>
-      </section>
 
-      {
-        console.log(allPostsData)
-      }
+
+      </section>
 
       <section className={utilStyles.headingMd}></section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
@@ -32,11 +39,13 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
+                <Link href={`/posts/${id}`}>
+                  <a>{title}</a>
+                </Link>
+                <br />
+                <small className={utilStyles.lightText}>
+                  <Date dateString={date} />
+                </small>
             </li>
           ))}
         </ul>
